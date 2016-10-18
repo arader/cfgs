6572a7ac5e3b4ee54b7792752046b5aa857dcdd8
Andrew Rader <ardr@outlook.com>
2016-10-19 07:44:42 +0800

    Fix cpubar.sh to handle missing sysctls

--- scripts/tmux/cpubar.sh
+++ scripts/tmux/cpubar.sh
@@ -9,24 +9,31 @@ temp_sum=0
 i=0
 while [ $i -lt $cpu_count ]
 do
-    temp=$(sysctl -n dev.cpu.$i.temperature | sed 's/C$//')
-    temp_sum=$(echo "$temp_sum + $temp" | bc -l)
     i=$(echo "$i + 1" | bc -l)
+    temp=$(sysctl -n dev.cpu.$i.temperature 2>/dev/null | sed 's/C$//')
+
+    [ "$temp" != "" ] || continue
+
+    temp_sum=$(echo "$temp_sum + $temp" | bc -l)
 done
 
-temp_avg=$(echo "$temp_sum / $cpu_count" | bc -l | sed 's/\..*//')
-
-color="#[fg=#268bd2]"
-
-if [ $(echo "$temp_avg >= 45" | bc -l) -eq 1 ]
+if [ $temp_sum != 0 ]
 then
+    temp_avg=$(echo "$temp_sum / $cpu_count" | bc -l | sed 's/\..*//')
+
+    if [ $(echo "$temp_avg >= 45" | bc -l) -eq 1 ]
+    then
+        color="#[fg=#dc022f]"
+    elif [ $(echo "$temp_avg >= 40" | bc -l) -eq 1 ]
+    then
+        color="#[fg=#cb4b16]"
+    elif [ $(echo "$temp_avg >= 35" | bc -l) -eq 1 ]
+    then
+        color="#[fg=#d33682]"
+    fi
+else
     color="#[fg=#dc022f]"
-elif [ $(echo "$temp_avg >= 40" | bc -l) -eq 1 ]
-then
-    color="#[fg=#cb4b16]"
-elif [ $(echo "$temp_avg >= 35" | bc -l) -eq 1 ]
-then
-    color="#[fg=#d33682]"
+    temp_avg="NA"
 fi
 
 echo -n "$bracket_color┌ $color$temp_avg$unit_color° $bracket_color┐"
